74e890c01c91e02b55e65864045abc0b758b126c
Stephen <stephen_h369@live.co.uk>
2020-07-03 16:48:48 +0100

    added save,update,delete methods to Film

--- console.rb
+++ console.rb
@@ -10,20 +10,42 @@ customer1 = Customer.new({
     'name' => 'Stephen',
     'funds' => 50
 })
+customer2 = Customer.new({
+    'name' => 'Erin',
+    'funds' => 100
+})
 
 
+film1 = Film.new({
+    'title' => 'The Fast And The Furious',
+    'price' => 12
+})
+film2 = Film.new({
+    'title' => 'Blade Runner 2049',
+    'price' => 15
+})
 
 
 
 # SAVES TO DB
 customer1.save()
+customer2.save()
+
+film1.save()
+film2.save()
+
 
 # UPDATES TO DB
 # customer1.funds = 100
 # customer1.update()
 
+# film1.price = 15
+# film1.update()
+
+
 # DELETES TO SINGLE ENTRIES
 # customer1.delete()
+# film1.delete()
 
 binding.pry
 nil
--- models/Film.rb
+++ models/Film.rb
@@ -11,4 +11,23 @@ class Film
         @price = options['price'].to_i
     end
 
+    def save()
+        sql = "INSERT INTO films (title, price) VALUES ($1, $2) RETURNING id"
+        values = [@title, @price]
+        film = SqlRunner.run(sql, values).first
+        @id = film['id'].to_i
+    end
+
+    def update()
+        sql = "UPDATE films SET (title, price) = ($1, $2) WHERE id = $3"
+        values = [@title, @price, @id]
+        SqlRunner.run(sql, values)
+    end
+
+    def delete()
+        sql = "DELETE FROM films WHERE id = $1"
+        values = [@id]
+        SqlRunner.run(sql, values)
+    end
+
 end
