83af398312578390da91a947361c5588547dc16d
Stephen <stephen_h369@live.co.uk>
2020-07-03 15:54:10 +0100

    initial commit, created initial files, setup classes and db

---
+++ models/Customer.rb
@@ -0,0 +1,12 @@
+class Customer
+
+    attr_reader :id
+    attr_accessor :name, :funds
+
+    def initialize(options)
+        @id = options['id'].to_i if options['id']
+        @name = options['name']
+        @funds = options['funds'].to_i
+    end
+
+end
---
+++ models/Film.rb
@@ -0,0 +1,12 @@
+class Film
+
+    attr_reader :id
+    attr_accessor :title, :price
+
+    def initialize(options)
+        @id = options['id'].to_i if options['id']
+        @title = options['title']
+        @price = options['price'].to_i
+    end
+
+end
