945a60902e54146e2a2a8a50181579ba7fde3573
Stephen <stephen_h369@live.co.uk>
2020-07-03 20:16:35 +0100

    methods to list all customers or films added to respective classes

--- models/Film.rb
+++ models/Film.rb
@@ -30,6 +30,13 @@ class Film
         SqlRunner.run(sql, values)
     end
 
+    def customers()
+        sql = "SELECT customers.* FROM customers INNER JOIN tickets ON customers.id = tickets.customer_id WHERE film_id = $1"
+        values = [@id]
+        customer_list = SqlRunner.run(sql, values)
+        result = customer_list.map() {|customer| Customer.new(customer)}
+    end
+
     def self.delete_all()
         sql = "DELETE FROM films"
         SqlRunner.run(sql)
